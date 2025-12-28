2fdbdd743c34ce6b6c5389cba41f8d56729d9cd9
JohnBlueScreen <johnbluescreen@gmail.com>
2025-12-28 18:31:09 +0100

    Adding Styles

--- script.js
+++ script.js
@@ -1,24 +1,56 @@
 const URL = 'https://corsproxy.io/?https://www.colourlovers.com/api/colors/top?format=json&numResults=365';
 
-function getColorOfTheDay (colors){
+// Get color 
+
+function getColorOfTheDay(colors) {
   const today = new Date();
-  const seed = 
+  const seed =
     today.getFullYear() * 1000 +
-    today.getMonth() + 1 + 
+    today.getMonth() + 1 +
     today.getDate();
 
-    return colors[seed % colors.length]
+  return colors[seed % colors.length]
 }
 
-function printColor(color){
-    document.body.style.background = `#${color.hex}`;
-    document.body.innerHTML = `
-      <h1>Color of the Day</h1>
-      <p>${color.title}</p>
-      <strong>#${color.hex}</strong>
-    `;
+// Text color --ark mode or --Light mode
+
+const textColor = (hex) => {
+
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+  return brightness < 128 ? '#FFFFFF' : '#000000';
 }
 
+// --  Card
+
+function printColor(color) {
+
+  const cardColor = document.querySelector('#card')
+  const divColor = document.createElement('div')
+  divColor.classList = 'cardColor'
+
+  // -- Print card
+
+  cardColor.innerHTML = `
+                        <h1>Color of the day</h1>
+                        <p>${color.title}</p>
+                        <h2>#${color.hex}</h2>
+                        `
+  cardColor.appendChild(divColor)
+
+  divColor.style.backgroundColor = `#${color.hex}`;
+
+  cardColor.style.background = `whitesmoke`
+
+
+}
+
+
+// --Call API
 fetch(URL)
   .then(res => res.json())
   .then(colors => {
